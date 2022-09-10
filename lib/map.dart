a2605bcecce40355a5cf2ec2782e087292283ab7
Victor Porton <porton.victor@gmail.com>
2022-09-10 17:58:05 +0300

    misc

--- lib/map.dart
+++ lib/map.dart
@@ -3,6 +3,7 @@ import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:location/location.dart';
 
 class MapSample extends StatefulWidget {
   @override
@@ -12,30 +13,38 @@ class MapSample extends StatefulWidget {
 class MapSampleState extends State<MapSample> {
   final Completer<GoogleMapController> _controller = Completer();
 
-  static const CameraPosition _kGooglePlex = CameraPosition(
-    target: LatLng(37.42796133580664, -122.085749655962),
-    zoom: 14.4746,
-  );
+  final location = Location();
+  LatLng? userLocation = LatLng(10, 20);
 
-  static const CameraPosition _kLake = CameraPosition(
-      bearing: 192.8334901395799,
-      target: LatLng(37.43296265331129, -122.08832357078792),
-      tilt: 59.440717697143555,
-      zoom: 19.151926040649414);
+  @override
+  void initState() {
+    super.initState();
+    location.onLocationChanged.listen(onLocationDataChanged);
+    location.getLocation().then(onLocationDataChanged);
+  }
+
+  void onLocationDataChanged(LocationData currentLocation) {
+    setState(() {
+      if(currentLocation.latitude == null || currentLocation.longitude == null) {
+        // userLocation = null;
+      } else {
+        setState(() {
+          userLocation = LatLng(currentLocation.latitude!, currentLocation.longitude!);
+        });
+      }
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
-    return const GoogleMap(
-      // onMapCreated: _onMapCreated,
-      initialCameraPosition: CameraPosition(
-        target: LatLng(45.521563, -122.677433),
-        zoom: 11.0,
-      ),
-    );
-  }
-
-  Future<void> _goToTheLake() async {
-    final GoogleMapController controller = await _controller.future;
-    controller.animateCamera(CameraUpdate.newCameraPosition(_kLake));
+    return userLocation == null
+        ? const Center(child: Text("Determining user location"))
+        : GoogleMap(
+          // onMapCreated: _onMapCreated,
+          initialCameraPosition: CameraPosition(
+            target: userLocation!,
+            zoom: 11.0,
+          ),
+        );
   }
 }
